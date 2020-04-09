df03b5e94daa72888bc00f880e8aee8b7ddebed7
Guillaume HACHE <hache.gui@gmail.com>
2020-04-09 19:01:32 +0200

    maj models.py

--- app/core/models.py
+++ app/core/models.py
@@ -12,3 +12,15 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that support using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
